c56eb1d77342190182cc536128f281e7d73b6000
RCarruthers <cruddyr@live.co.uk>
2017-11-09 08:59:57 +0000

    Added the processors to the routes. Set a header in the exchange to differentiate between different request paths.

--- src/main/java/com/ryan/uom/RentalCarsTest/routes/MainRoute.java
+++ src/main/java/com/ryan/uom/RentalCarsTest/routes/MainRoute.java
@@ -2,11 +2,15 @@ package com.ryan.uom.RentalCarsTest.routes;
 
 import com.ryan.uom.RentalCarsTest.model.VehicleList;
 import com.ryan.uom.RentalCarsTest.processors.SortByPriceProcessor;
+import com.ryan.uom.RentalCarsTest.processors.SortByRatingsPerCarTypeProcessor;
+import com.ryan.uom.RentalCarsTest.processors.SortBySippProcessor;
+import com.ryan.uom.RentalCarsTest.processors.SortBySupplierRatingProcessor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import static com.ryan.uom.RentalCarsTest.util.Constants.ENDPOINT_START_ROUTE;
+
+import static com.ryan.uom.RentalCarsTest.util.Constants.*;
 
 @Component
 public class MainRoute extends RouteBuilder {
@@ -14,14 +18,43 @@ public class MainRoute extends RouteBuilder {
     @Autowired
     private SortByPriceProcessor priceProcessor;
 
+    @Autowired
+    private SortBySippProcessor sippProcessor;
+
+    @Autowired
+    private SortByRatingsPerCarTypeProcessor ratingsProcessor;
+
+    @Autowired
+    private SortBySupplierRatingProcessor supplierRatingProcessor;
+
     @Override
     public void configure() throws Exception {
 
         from(ENDPOINT_START_ROUTE)
-                .to("http4://www.rentalcars.com/js/vehicles.json")
+                .setHeader(HEADER_REQUEST_PATH, body())
+                .to(ENDPOINT_JSON_LOCATION)
                 .unmarshal().json(JsonLibrary.Jackson, VehicleList.class)
+                .choice()
+                .when(header(HEADER_REQUEST_PATH).isEqualTo(REQUEST_PATH_PRICE)).to(ENDPOINT_SORT_BY_PRICE)
+                .when(header(HEADER_REQUEST_PATH).isEqualTo(REQUEST_PATH_SIPP)).to(ENDPOINT_SORT_BY_SIPP)
+                .when(header(HEADER_REQUEST_PATH).isEqualTo(REQUEST_PATH_RATING)).to(ENDPOINT_SORT_BY_CAR_TYPE_RATING)
+                .when(header(HEADER_REQUEST_PATH).isEqualTo(REQUEST_PATH_SCORE)).to(ENDPOINT_SORT_BY_SUPPLIER_RATING)
+                .end();
+
+        from(ENDPOINT_SORT_BY_PRICE)
                 .process(priceProcessor)
-                .log("${body}")
+                .end();
+
+        from(ENDPOINT_SORT_BY_SIPP)
+                .process(sippProcessor)
+                .end();
+
+        from(ENDPOINT_SORT_BY_CAR_TYPE_RATING)
+                .process(ratingsProcessor)
+                .end();
+
+        from(ENDPOINT_SORT_BY_SUPPLIER_RATING)
+                .process(supplierRatingProcessor)
                 .end();
 
     }
--- src/main/java/com/ryan/uom/RentalCarsTest/util/Constants.java
+++ src/main/java/com/ryan/uom/RentalCarsTest/util/Constants.java
@@ -2,4 +2,19 @@ package com.ryan.uom.RentalCarsTest.util;
 
 public class Constants {
     public static final String ENDPOINT_START_ROUTE = "direct:mainRoute";
+    public static final String ENDPOINT_SORT_BY_PRICE = "direct:sortByPriceRoute";
+    public static final String ENDPOINT_SORT_BY_SIPP = "direct:sortBySippRoute";
+    public static final String ENDPOINT_SORT_BY_CAR_TYPE_RATING = "direct:sortByCarTypeRating";
+    public static final String ENDPOINT_SORT_BY_SUPPLIER_RATING = "direct:sortBySupplierRating";
+    public static final String ENDPOINT_JSON_LOCATION = "http4://www.rentalcars.com/js/vehicles.json";
+
+    public static final String HEADER_REQUEST_PATH = "headerRequestPath";
+    public static final String REQUEST_PATH_PRICE = "/getVehiclesByPrice";
+    public static final String REQUEST_PATH_SIPP = "/getVehiclesBySIPP";
+    public static final String REQUEST_PATH_RATING = "/getVehiclesByRating";
+    public static final String REQUEST_PATH_SCORE = "/getVehiclesByScore";
+
+
+    public static final String LINE_SEPERATOR = System.getProperty("line.separator");
+
 }
